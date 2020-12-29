758deafff3dda0f50c60bc103d75d1ea047f6916
Ryan <rypoon18@gmail.com>
2020-12-29 11:52:02 -0500

    reduce to 10 listings, omit combos

--- components/AllListings/AllListings.js
+++ components/AllListings/AllListings.js
@@ -2,8 +2,23 @@ import { useRouter } from 'next/router';
 import Listing from '../Listing/Listing';
 import Suggestions from '../Suggestions/Suggestions';
 
-const createListing = (e, i) => {
-    const title = e.title;
+
+const isCombo = (str) => {
+    let omit = ['combo', 'bundle', 'plus', 'with', 'and', '+']
+    let combo;
+    for (const o of omit) {
+        combo ||= str.toLowerCase().indexOf(o) != -1
+    }
+    return combo;
+}
+
+
+const createListing = (e, i, combo) => {
+
+
+    const title = e.title[0];
+    if (!combo && isCombo(title)) return[null, null];
+
     const galleryURL = e.galleryURL ? e.galleryURL[0]: 'Unavailable';
     const viewItemURL = e.viewItemURL ? e.viewItemURL[0] : 'Unavailable';
     const convertedCurrentPrice = e.sellingStatus[0].convertedCurrentPrice[0]['__value__'];
@@ -17,23 +32,32 @@ const createListing = (e, i) => {
     return [parseFloat(price), <Listing key={i} title={title} galleryURL={galleryURL} viewItemURL={viewItemURL} convertedCurrentPrice={price} condition={condition}/>];
 }
 
+const getGeneralData = (prices) => {
+    const min = Math.min(...prices);
+    const max = Math.max(...prices);
+    const median = prices.sort()[Math.floor(prices.length/ 2)];
+    const mean = Math.floor(prices.reduce((a, e) => a + e) / prices.length * 100) / 100;
+    const targetPrice = calcTarget(mean, median);
+    return [min, max, median, mean, targetPrice];
+}
+
+
+
 const calcTarget = (mean, median) => Math.round(((median * 0.8) + (mean * 0.2)) * 100) /100;
 
 const AllListings = (props) => {
     const router = useRouter();
 
     if (props.listings) {
-        let prices = [];
-        const listings = props.listings.map((e, i) => {
-            const [p, l] = createListing(e, i);
-            prices.push(p);
+        let allPrices = [];
+        let combo = isCombo(props.query);
+        const listings = props.listings.slice(0, 11).map((e, i) => {
+            const [p, l] = createListing(e, i, combo);
+            if (p != null) allPrices.push(p);
             return l;
         });
-        const min = Math.min(...prices);
-        const max = Math.max(...prices);
-        const median = prices[Math.floor(prices.length/ 2)];
-        const mean = Math.floor(prices.reduce((a, e) => a + e) / prices.length * 100) / 100;
-        const targetPrice = calcTarget(mean, median);
+        
+        const [min, max, median, mean, targetPrice] = getGeneralData(allPrices);
 
         return (
             <div>
--- components/Directions/Directions.js
+++ components/Directions/Directions.js
@@ -7,7 +7,7 @@ const Directions = () => {
                 This calculator will give a rough approximate of a searched item's resale value.
                 Listings straight from Ebay are used to calculate a suggested price that you should look for if you are buying, 
                 or a potential selling price if you are selling!
-                The target resale price is the average of the mean and median of the top 100 
+                The target resale price is the average of the mean and median of the top 10 
                 Ebay listings of items matching the keywords that were searched for.
             </h2>
         </div>
