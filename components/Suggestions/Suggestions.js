31ab8c4119a4510a2ca2ee7cba7e083969c6629e
Ryan <rypoon18@gmail.com>
2020-12-29 12:52:59 -0500

    change details styling

--- components/Suggestions/Suggestions.js
+++ components/Suggestions/Suggestions.js
@@ -4,15 +4,17 @@ const Suggestions = ({ query, min, max, mean, median, targetPrice }) => {
     
     return (
         <div className={style.container}>
-            <div>
+            {/* <div> */}
                 <h2>Results for {query}:</h2>
-                <p>Average price: <span className={style.price}>${ mean }</span></p>
-                <p>Median Price: <span className={style.price}>${ median }</span></p>
-                <p>Lowest Offer: <span className={style.price}>${ min }</span></p>
-                <p>Highest Offer: <span className={style.price}>${ max }</span></p>
-                <h2>TARGET SELLING PRICE:</h2> <span className={style.price}>${ targetPrice }</span>
-            </div>
-            <img src="/undraw_Online_information_re_erks.svg" alt="resale value calculator details"></img>
+                <div className={style['metrics-container']}>
+                    <div className={style.metric}><span className={style.price}>{ targetPrice }</span>Target</div>
+                    <div className={style.metric}><span className={style.price}>{ mean }</span>Average</div>
+                    <div className={style.metric}><span className={style.price}>{ median }</span>Median</div>
+                    <div className={style.metric}><span className={style.price}>{ min }</span>Lowest</div>
+                    <div className={style.metric}><span className={style.price}>{ max }</span>Highest</div>
+                    {/* <img className={style.drawing}src="/undraw_Online_information_re_erks.svg" alt="resale value calculator details"></img> */}
+                </div>
+            {/* </div> */}
         </div>
     )
 }
