3d655466dd3a18a6854aa4144f403b0c190000b2
Ryan <rypoon18@gmail.com>
2020-12-29 17:16:58 -0500

    add quotes around prev query

--- components/Suggestions/Suggestions.js
+++ components/Suggestions/Suggestions.js
@@ -5,17 +5,14 @@ const Suggestions = ({ query, min, max, mean, median, targetPrice }) => {
     const targetClass=`${style.metric} ${style['target-sub']}`;
     return (
         <div className={style.container}>
-            {/* <div> */}
-                <h2>Results for {query}:</h2>
-                <div className={style['metrics-container']}>
-                    <div className={targetClass}><span className={style.price}>{ targetPrice }</span>Target</div>
-                    <div className={style.metric}><span className={style.price}>{ mean }</span>Average</div>
-                    <div className={style.metric}><span className={style.price}>{ median }</span>Median</div>
-                    <div className={style.metric}><span className={style.price}>{ min }</span>Lowest</div>
-                    <div className={style.metric}><span className={style.price}>{ max }</span>Highest</div>
-                    {/* <img className={style.drawing}src="/undraw_Online_information_re_erks.svg" alt="resale value calculator details"></img> */}
-                </div>
-            {/* </div> */}
+            <h2>Results for "{query}"":</h2>
+            <div className={style['metrics-container']}>
+                <div className={targetClass}><span className={style.price}>{ targetPrice }</span>Target</div>
+                <div className={style.metric}><span className={style.price}>{ mean }</span>Average</div>
+                <div className={style.metric}><span className={style.price}>{ median }</span>Median</div>
+                <div className={style.metric}><span className={style.price}>{ min }</span>Lowest</div>
+                <div className={style.metric}><span className={style.price}>{ max }</span>Highest</div>
+            </div>
         </div>
     )
 }
