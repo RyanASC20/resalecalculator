be2b9753005ac78ae6e3b34293b99b066de51b64
Ryan <rypoon18@gmail.com>
2020-12-29 10:38:55 -0500

    change heading

--- components/Directions/Directions.js
+++ components/Directions/Directions.js
@@ -2,7 +2,7 @@ import style from './Directions.module.scss';
 const Directions = () => {
     return (
         <div className={style['directions-container']}>
-            <h1 className={style["welcome-message"]}>Welcome to Resale Value Calculator!</h1>
+            <h1 className={style["welcome-message"]}>Welcome to the Cycle Resale Value Calculator!</h1>
             <h2 className={style["directions"]}>
                 This calculator will give a rough approximate of a searched item's resale value.
                 Listings straight from Ebay are used to calculate a suggested price that you should look for if you are buying, 
