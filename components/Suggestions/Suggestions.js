a6376ffd93994aafc931ae4a76abcad41ee64f5c
Ryan <rypoon18@gmail.com>
2020-12-29 13:46:34 -0500

    add color to target price

--- components/Suggestions/Suggestions.js
+++ components/Suggestions/Suggestions.js
@@ -2,12 +2,13 @@ import style from './Suggestions.module.scss';
 
 const Suggestions = ({ query, min, max, mean, median, targetPrice }) => {
     
+    const targetClass=`${style.metric} ${style['target-sub']}`;
     return (
         <div className={style.container}>
             {/* <div> */}
                 <h2>Results for {query}:</h2>
                 <div className={style['metrics-container']}>
-                    <div className={style.metric}><span className={style.price}>{ targetPrice }</span>Target</div>
+                    <div className={targetClass}><span className={style.price}>{ targetPrice }</span>Estimated Target Price</div>
                     <div className={style.metric}><span className={style.price}>{ mean }</span>Average</div>
                     <div className={style.metric}><span className={style.price}>{ median }</span>Median</div>
                     <div className={style.metric}><span className={style.price}>{ min }</span>Lowest</div>
