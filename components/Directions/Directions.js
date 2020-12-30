8f4427c66e6ca99e42c67d997217367bdc6b0483
Ryan <rypoon18@gmail.com>
2020-12-30 16:03:12 -0500

    change desc

--- components/Directions/Directions.js
+++ components/Directions/Directions.js
@@ -5,7 +5,7 @@ const Directions = () => {
             <h1 className={style["welcome-message"]}>Welcome to the Cycle Resale Value Calculator!</h1>
             <h2 className={style["directions"]}>
                 This calculator will give a rough approximate of a searched item's resale value.
-                Listings straight from Ebay are used to calculate a suggested price that you should look for if you are buying, 
+                Listings straight from Ebay are used to calculate a suggested price that you should aim to pay if you are buying
                 or a potential selling price if you are selling!
                 The target resale price is an estimated desirable pricepoint where your item is likely to be purchased. It is calculated using
                 the top 10 Ebay listings of items matching the keywords that were searched for.
